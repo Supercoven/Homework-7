525a12f5b56541934c0d78a6482e493dbe56a222
Burundukov Denis <burundukov.d@yandex.ru>
2022-12-05 16:33:53 +0500

    Task 47 Completed

---
+++ Task 47/Program.cs
@@ -0,0 +1,30 @@
+﻿
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        double[,] Random2DArray(int collumns, int rows)
+        {
+            Console.WriteLine("Введите минимальное число: ");
+            double min = Math.Round(double.Parse(Console.ReadLine()));
+            Console.WriteLine("Введите максимальное число: ");
+            double max = Math.Round(double.Parse(Console.ReadLine()));
+            double[,] arr = new double[rows, collumns];
+            for (int i = 0; i < rows; i++)
+            {
+                for (int j = 0; j < collumns; j++)
+                {
+                    arr[i, j] = Math.Round(new
+                        Random().NextDouble() * (-max - min) + min);
+
+                    Console.Write(arr[i, j] + " ");
+                }
+
+                Console.WriteLine();
+            }
+            return arr;
+        }
+
+        Random2DArray(3, 4);
+    }
+}
